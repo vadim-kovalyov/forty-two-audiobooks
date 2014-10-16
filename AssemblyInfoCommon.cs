8b7b0f69dad9124eff1b113c92359f0c202f139b
Vadim Kovalyov <vadim.kovalyov@gmail.com>
2014-10-16 19:46:16 -0400

    Initial commit: Basic project structure

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Resources;
+
+
+// Common assembly information.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FortyTwoAudiobooks")]
+[assembly: AssemblyCopyright("Copyright © Vadim Kovalyov 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Common assembly versioning.
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
